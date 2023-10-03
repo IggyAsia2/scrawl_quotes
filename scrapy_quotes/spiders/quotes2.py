1d16d864a38a1224da8d4e646d8ffffb26a7a96a
root <root@vultr.guest>
2023-10-03 10:22:22 +0000

    a

--- scrapy_quotes/spiders/quotes2.py
+++ scrapy_quotes/spiders/quotes2.py
@@ -1,10 +1,19 @@
+# scrapy_quotes/scrapy_quotes/spiders/quotes.py
 import scrapy
 
-
-class Quotes2Spider(scrapy.Spider):
-    name = "quotes2"
-    allowed_domains = ["trangvangvietnam.com"]
-    start_urls = ["http://trangvangvietnam.com/"]
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['trangvangvietnam.com']
+    start_urls = ['https://trangvangvietnam.com/categories/129310/xuat-nhap-khau-cac-cong-ty-xuat-nhap-khau.html']
 
     def parse(self, response):
-        pass
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css("span.text::text").extract_first(),
+                'author': quote.css("small.author::text").extract_first(),
+                'tags': ','.join(quote.css("div.tags > a.tag::text").extract())
+            }
+
+        next_page_url = response.css("li.next > a::attr(href)").extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
