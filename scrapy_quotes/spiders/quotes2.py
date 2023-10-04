f4687f2bce437f7a1c15ec22efe8a00ea3c7a4d9
root <root@vultr.guest>
2023-10-04 01:49:45 +0000

    a

--- scrapy_quotes/spiders/quotes2.py
+++ scrapy_quotes/spiders/quotes2.py
@@ -7,13 +7,13 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['https://trangvangvietnam.com/categories/129310/xuat-nhap-khau-cac-cong-ty-xuat-nhap-khau.html']
 
     def parse(self, response):
-        for quote in response.css("div.quote"):
+        for quote in response.css("div.w-100 h-auto shadow rounded-3 bg-white p-2 mb-3"):
             yield {
-                'text': quote.css("span.text::text").extract_first(),
-                'author': quote.css("small.author::text").extract_first(),
-                'tags': ','.join(quote.css("div.tags > a.tag::text").extract())
+                'company': quote.css("div.listings_center h2 a::text").extract_first(),
+                # 'author': quote.css("small.author::text").extract_first(),
+                # 'tags': ','.join(quote.css("div.tags > a.tag::text").extract())
             }
 
-        next_page_url = response.css("li.next > a::attr(href)").extract_first()
-        if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url))
+        # next_page_url = response.css("li.next > a::attr(href)").extract_first()
+        # if next_page_url is not None:
+        #     yield scrapy.Request(response.urljoin(next_page_url))
