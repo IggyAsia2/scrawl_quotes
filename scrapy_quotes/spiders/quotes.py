c0be19fa18c69020cee816aa7aeb47b58d4a61b9
root <root@vultr.guest>
2023-10-04 04:40:10 +0000

    a

--- scrapy_quotes/spiders/quotes.py
+++ scrapy_quotes/spiders/quotes.py
@@ -9,22 +9,22 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.css("div.bg-white"):
             company =  quote.css("div.listings_center a::text").extract_first()
-            # email = quote.css("div.email_web_section a::attr(href)").extract_first()
-            # address = quote.css("div.logo_congty_diachi small::text").extract_first()
-            # phone = quote.css("div.listing_dienthoai a::text").extract_first()
-            # sponsor = quote.css("span.star_text::text").extract_first()
-            # major = quote.css("span.nganh_listing_txt::text").extract_first()
-            # website = quote.css("div.email_web_section a::attr(href)")[-1].extract()
+            email = quote.css("div.email_web_section a::attr(href)").extract_first()
+            address = quote.css("div.logo_congty_diachi small::text").extract_first()
+            phone = quote.css("div.listing_dienthoai a::text").extract_first()
+            sponsor = quote.css("span.star_text::text").extract_first()
+            major = quote.css("span.nganh_listing_txt::text").extract_first()
+            website = quote.css("div.email_web_section a:nth-child(2)::attr(href)").extract_first()
             
             if company:
                 yield {
                     'company': company,
-                    # 'email': email.split(":")[1],
-                    # 'address': address,
-                    # 'phone': phone,
-                    # 'sponsor': sponsor or "",
-                    # 'major': major,
-                    # 'website': website
+                    'email': email.split(":")[1],
+                    'address': address,
+                    'phone': phone,
+                    'sponsor': sponsor,
+                    'major': major,
+                    'website': website
                 }
 
         next_page_url = response.xpath('//div[@id="paging"]/a/@href')[-1].extract()
