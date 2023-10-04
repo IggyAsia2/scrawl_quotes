7ef60fe939dff878fe295b134a8b428ad4a797bf
root <root@vultr.guest>
2023-10-04 04:02:27 +0000

    a

--- scrapy_quotes/spiders/quotes.py
+++ scrapy_quotes/spiders/quotes.py
@@ -9,9 +9,14 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.css("div.bg-white"):
             company =  quote.css("div.listings_center a::text").extract_first()
+            sponsor = quote.css("span.star_text::text").extract_first()
+            # email = quote.css("div.listings_center a::text").extract_first()
+            
             if company:
                 yield {
                     'company': company,
+                    'sponsor': sponsor or "",
+                    # 'email': email,
                     # 'author': quote.css("small.author::text").extract_first(),
                     # 'tags': ','.join(quote.css("div.tags > a.tag::text").extract())
                 }
